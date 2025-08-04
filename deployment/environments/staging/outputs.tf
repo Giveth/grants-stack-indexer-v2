044357cba077fbfd0eac538764ad740efbc24008
geleeroyale <krassvs@gmail.com>
2025-08-04 15:33:32 +0200

    add terraform outputs needed for restore db workflow

--- deployment/environments/staging/outputs.tf
+++ deployment/environments/staging/outputs.tf
@@ -9,3 +9,37 @@ output "deployment_state" {
 output "api_gateway_url" {
   value = module.api_gateway.api_gateway_url
 }
+
+# Outputs required by restore database workflow
+output "rds_endpoint" {
+  value = module.storage.rds_endpoint
+}
+
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "public_subnets" {
+  value = module.networking.public_subnets
+}
+
+output "processing_security_group_id" {
+  value = module.networking.processing_security_group_id
+}
+
+# Additional useful outputs for blue/green deployment
+output "private_subnets" {
+  value = module.networking.private_subnets
+}
+
+output "rds_security_group_id" {
+  value = module.networking.rds_security_group_id
+}
+
+output "api_security_group_id" {
+  value = module.networking.api_security_group_id
+}
+
+output "load_balancer_security_group_id" {
+  value = module.networking.load_balancer_security_group_id
+}
